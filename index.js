1f04261c09a979859be70a77eb6c05006bb32153
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-var typeset = require('typeset');
+var typeset = require('typeset')
 
-exports.name = 'typeset';
-exports.inputFormats = ['typeset', 'typesetbar'];
-exports.outputFormat = 'html';
+exports.name = 'typeset'
+exports.inputFormats = ['typeset', 'typesetbar']
+exports.outputFormat = 'html'
 
 exports.render = function (str, options) {
-  return typeset(str, options);
-};
+  return typeset(str, options)
+}
